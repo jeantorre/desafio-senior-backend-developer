6d635dde2d99382e461c0ebd359dfe5fc5d82a9b
Jean Torre <jean.torre21@gmail.com>
2025-05-06 00:44:50 -0300

    commit inicial - estabelecimento da conexao com o banco de dados

---
+++ backend/database.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+load_dotenv()
+
+POSTGRES_DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(POSTGRES_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    """
+    Função geradora que fornece uma sessão do banco de dados.
+
+    :return: Uma sessão do banco de dados.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
