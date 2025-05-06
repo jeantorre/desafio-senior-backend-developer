d464d1da48c841201c01e3f1c173463bd7baa022
Jean Torre <jean.torre21@gmail.com>
2025-05-06 19:29:19 -0300

    retirada um endpoint que nao estava sendo utilizado

--- backend/router/router_auth.py
+++ backend/router/router_auth.py
@@ -4,16 +4,14 @@ from crud import validar_senha, validar_usuario_por_email
 from database import get_db
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from model import ModeloUsuario
 from schema import Token
 from sqlalchemy.orm import Session
-from utils.auth import ALGORITHM, SECRET_KEY, criar_token_acesso, oauth2_scheme
+from utils.auth import criar_token_acesso
 
 router_auth = APIRouter(prefix="/auth", tags=["Autenticação"])
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-REFRESH_TOKEN_EXPIRE_DAYS = 7
+EXPIRA_TOKEN_ACESSO_MINUTOS = 30
+EXPIRA_TOKEN_REFRESH_DIAS = 7
 
 
 @router_auth.post("/login", response_model=Token)
@@ -36,53 +34,19 @@ def login(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = criar_token_acesso(
-        dados_usuario={"sub": usuario.cpf}, tempo_expiracao=access_token_expires
+    expira_token_acesso = timedelta(minutes=EXPIRA_TOKEN_ACESSO_MINUTOS)
+    token_acesso = criar_token_acesso(
+        dados_usuario={"sub": usuario.email_usuario}, tempo_expiracao=expira_token_acesso
     )
 
-    refresh_token_expires = timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
-    refresh_token = criar_token_acesso(
-        dados_usuario={"sub": usuario.cpf, "token_type": "refresh"},
+    refresh_token_expires = timedelta(days=EXPIRA_TOKEN_REFRESH_DIAS)
+    token_refresh = criar_token_acesso(
+        dados_usuario={"sub": usuario.email_usuario, "token_type": "refresh"},
         tempo_expiracao=refresh_token_expires,
     )
 
     return {
-        "token_acesso": access_token,
-        "token_refresh": refresh_token,
+        "token_acesso": token_acesso,
+        "token_refresh": token_refresh,
         "tipo_token": "bearer",
     }
-
-
-@router_auth.post("/refresh-token", response_model=Token, include_in_schema=False)
-async def refresh_token(
-    current_token: str = Depends(oauth2_scheme),
-    db: Session = Depends(get_db),
-):
-    try:
-        payload = jwt.decode(current_token, SECRET_KEY, algorithms=[ALGORITHM])
-        if payload.get("token_type") != "refresh":
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="Token inválido"
-            )
-
-        cpf: str = payload.get("sub")
-        usuario = db.query(ModeloUsuario).filter(ModeloUsuario.cpf == cpf).first()
-
-        access_token = criar_token_acesso(
-            dados_usuario={
-                "sub": usuario.cpf,
-                "token_type": "access",
-            }
-        )
-
-        return {
-            "token_acesso": access_token,
-            "token_refresh": current_token,
-            "tipo_token": "bearer",
-        }
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token expirado ou inválido",
-        )
