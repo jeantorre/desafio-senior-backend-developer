0f336d7ca1da255301cf85036f8470e626d6b6db
Jean Torre <jean.torre21@gmail.com>
2025-05-08 00:26:37 -0300

    feat adicionado modelo de transacao

--- backend/model/__init__.py
+++ backend/model/__init__.py
@@ -1,10 +1,11 @@
 from .models_dim_transporte import ModeloTransporte
 from .models_rl_usuario_documento import ModeloRlUsuarioDocumento
-from .relationship import ModeloDocumento, ModeloUsuario
+from .relationship import ModeloDocumento, ModeloTransacaoCarteira, ModeloUsuario
 
 __all__ = [
     "ModeloUsuario",
     "ModeloDocumento",
     "ModeloRlUsuarioDocumento",
     "ModeloTransporte",
+    "ModeloTransacaoCarteira",
 ]
