134b85ca9493c0dc074ef113bc6d9c24eff5847e
Jean Torre <jean.torre21@gmail.com>
2025-05-08 00:26:58 -0300

    feat adicionado modelo de transacao

--- backend/model/relationship.py
+++ backend/model/relationship.py
@@ -2,10 +2,25 @@ from sqlalchemy.orm import relationship
 
 from .models_dim_documento import ModeloDocumento
 from .models_dim_usuario import ModeloUsuario
+from .models_fat_transacao_carteira import ModeloTransacaoCarteira
 
 ModeloUsuario.documentos = relationship(
     "ModeloDocumento", back_populates="usuarios", secondary="rl_usuario_documento"
 )
 ModeloDocumento.usuarios = relationship(
     "ModeloUsuario", back_populates="documentos", secondary="rl_usuario_documento"
+)
+
+ModeloTransacaoCarteira.usuario = relationship(
+    "ModeloUsuario", back_populates="transacoes"
+)
+ModeloUsuario.transacoes = relationship(
+    "ModeloTransacaoCarteira", back_populates="usuario"
+)
+
+ModeloTransacaoCarteira.documento = relationship(
+    "ModeloDocumento", back_populates="transacoes"
+)
+ModeloDocumento.transacoes = relationship(
+    "ModeloTransacaoCarteira", back_populates="documento"
 )
