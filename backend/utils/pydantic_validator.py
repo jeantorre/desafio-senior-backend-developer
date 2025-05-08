be1893288e9246c66237ce0614995af5dc334b45
jeantorre <jean.torre21@gmail.com>
2025-05-08 17:47:38 -0300

    retirada funcao inutilizada

--- backend/utils/pydantic_validator.py
+++ backend/utils/pydantic_validator.py
@@ -16,19 +16,3 @@ def maiuscula_sem_acento(valor: str) -> str:
         valor = re.sub(r"[^a-zA-Z0-9\s,]", "", valor)
         return valor.upper()
     return valor
-
-
-def validador_numero(valor):
-    """
-    Valida se o valor é uma string e se contém apenas dígitos.
-
-    param: valor - valor a ser validado
-    return: valor após validação
-    """
-    if valor is None:
-        return valor
-    if not isinstance(valor, str):
-        raise ValueError("Número precisa ser uma string")
-    if not valor.isdigit():
-        raise ValueError("Número precisa conter apenas dígitos")
-    return valor
