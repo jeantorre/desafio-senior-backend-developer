ef540645375debe92a88c6b57c6f35c9a7acfd76
Jean Torre <jean.torre21@gmail.com>
2025-05-09 00:54:28 -0300

    feat adicionada check na API

--- backend/router/__init__.py
+++ backend/router/__init__.py
@@ -1,5 +1,6 @@
 from .router_auth import router_auth
 from .router_documento import router_documento
+from .router_test_backend import router_teste_backend
 from .router_transacao import router_transacao
 from .router_transporte import router_transporte
 from .router_usuario import router_usuario
@@ -10,4 +11,5 @@ __all__ = [
     "router_documento",
     "router_transporte",
     "router_transacao",
+    "router_teste_backend",
 ]
